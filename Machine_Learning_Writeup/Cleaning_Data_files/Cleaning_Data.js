730a835a5ad249570cc392a50d40c70a228c2950
jsyoungnet <jyoung@Jeffreys-MacBook-Pro.local>
2017-03-02 23:55:03 +1100

    First Commit of Web Writeup

---
+++ Machine_Learning_Writeup/Cleaning_Data_files/Cleaning_Data.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20170302
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Cleaning_Data_files/Cleaning_DataMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
